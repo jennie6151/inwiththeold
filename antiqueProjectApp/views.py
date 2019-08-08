5cbd5d2ef855acafe17c45b64c74eac4b0f31de0
jennie6151 <jennie6151@hotmail.com>
2019-08-08 16:29:51 +0100

    Homepage visit count added

--- antiqueProjectApp/views.py
+++ antiqueProjectApp/views.py
@@ -8,10 +8,14 @@ def index(request):
 
     numAntiques = Antique.objects.all().count()
     numCreators = Creator.objects.count()
+
+    numVisits = request.session.get('numVisits', 0)
+    request.session['numVisits'] = numVisits + 1
     
     context = {
         'numAntiques': numAntiques,
         'numCreators': numCreators,
+        'numVisits': numVisits,
     }
 
     return render(request, 'index.html', context=context)
@@ -20,14 +24,15 @@ from django.views import generic
 
 class AntiqueListView(generic.ListView):
     model = Antique
-    paginate_by = 12
+    paginate_by = 2
 
 class AntiqueDetailView(generic.DetailView):
     model = Antique
 
 class CreatorListView(generic.ListView):
     model = Creator
-    paginate_by = 12
+    paginate_by = 2
 
 class CreatorDetailView(generic.DetailView):
     model = Creator
+    paginate_by = 2
