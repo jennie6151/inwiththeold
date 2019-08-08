38f9855f54901b33e9a6a1a5793dd3dafed99720
jennie6151 <jennie6151@hotmail.com>
2019-08-08 15:06:15 +0100

    creator_list and detail pulling from db

--- antiqueProjectApp/urls.py
+++ antiqueProjectApp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('Antiques/', views.AntiqueListView.as_view(), name='antiques'),
+  path('antiques/', views.AntiqueListView.as_view(), name='antiques'),
   path('Antique/<int:pk>', views.AntiqueDetailView.as_view(), name='antique-detail'),
-  path('Creators/', views.CreatorListView.as_view(), name='creators'),
+  path('creators/', views.CreatorListView.as_view(), name='creators'),
   path('Creator/<int:pk>', views.CreatorDetailView.as_view(), name='creator-detail'),
 ]
