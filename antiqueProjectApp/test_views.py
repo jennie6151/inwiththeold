de6ddb7c781200bb73081a2837d2ddd6fecbca9b
jennie6151 <jennie6151@hotmail.com>
2019-08-14 15:23:19 +0100

    draft test added to travis

--- antiqueProjectApp/test_views.py
+++ antiqueProjectApp/test_views.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from .models import Antique
 
-class TestViews(TestCase):
-    def test_get_home_page(self):
-        page = self.client.get("/")
-        self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "index.html")
+#class TestViews(TestCase):
+    #def test_get_home_page(self):
+       # page = self.client.get("/")
+        #self.assertEqual(page.status_code, 200)
+       # self.assertTemplateUsed(page, "index.html")
 
     #def test_get_antique_list_page(self):
        # page = self.client.get("/antiques")
