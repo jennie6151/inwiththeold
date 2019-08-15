02e1f09623ad061ea0f2d8629abc50a3c8b08c52
jennie6151 <jennie6151@hotmail.com>
2019-08-15 10:35:16 +0100

    secret_key hidden

--- antiqueProject/settings.py
+++ antiqueProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '03oma-8widk87u+_=1#!!6gvdae&-k87_+s-&0g%om4)lwap0r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
