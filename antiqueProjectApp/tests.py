2f0ee79894a66db94b2feb2e09b0e4351e778602
jennie6151 <jennie6151@hotmail.com>
2019-08-21 15:56:02 +0100

    Travis test updated

--- antiqueProjectApp/tests.py
+++ antiqueProjectApp/tests.py
@@ -4,6 +4,6 @@ from .models import Antique
 class AntiqueTests(TestCase):
     
     def test_str(self):
-        test_name = Antique(name = 'AntiqueName')
+        test_name = Antique(AntiqueName = 'AntiqueName')
         self. assertEqual(str(test_name), 'AntiqueName')
 
