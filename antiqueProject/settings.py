ffb774153f2b8bf5316bb1f8a7d8713d24edf2eb
jennie6151 <jennie6151@hotmail.com>
2019-08-23 17:30:12 +0100

    debug = true

--- antiqueProject/settings.py
+++ antiqueProject/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '1234')
  
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'inwiththeold.herokuapp.com']
 
